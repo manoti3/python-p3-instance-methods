250eb0516aa414feed9c7ae169378d327cec8250
manoti3 <hillarymanoti0@gmail.com>
2024-03-05 22:41:40 +0300

    completed assignment

--- lib/dog.py
+++ lib/dog.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 
 class Dog:
+    def bark(self):
+        print("Woof!")
+    def sit(self):
+        print("The dog is sitting.")
     # Class body goes here
 
     #Instance method definition
